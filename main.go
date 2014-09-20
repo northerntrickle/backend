183f16adf3cd07ea68ed0461f5dd0e205dc924b2
Zach Latta <zach@zachlatta.com>
2014-09-20 14:42:59 -0700

    Add hacky sign up and login.

--- main.go
+++ main.go
@@ -1,13 +1,75 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"code.google.com/p/go-uuid/uuid"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/websocket"
 )
+
+type database struct {
+	users map[string]*User
+}
+
+func (d *database) getUserByUsername(username string) (u *User, err error) {
+	for _, v := range d.users {
+		if v.Username == username {
+			return v, nil
+		}
+	}
+	return nil, errors.New("not found")
+}
+
+func NewDatabase() *database {
+	return &database{
+		users: make(map[string]*User),
+	}
+}
+
+var db = NewDatabase()
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"-"`
+}
+
+type JWT struct {
+	tok *jwt.Token
+}
+
+func (t *JWT) String() string {
+	str, err := t.tok.SignedString([]byte("foobar"))
+	if err != nil {
+		panic(err)
+	}
+	return str
+}
+
+func NewJWT(userID string) *JWT {
+	tok := jwt.New(jwt.GetSigningMethod("HS256"))
+	tok.Claims["id"] = userID
+	tok.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	return &JWT{tok}
+}
+
+func CreateUser(username, password string) *User {
+	user := &User{
+		ID:       uuid.NewUUID().String(),
+		Username: username,
+		Password: password,
+	}
+	db.users[user.ID] = user
+	return user
+}
 
 const (
 	writeWait      = 10 * time.Second
@@ -108,8 +170,65 @@ func main() {
 		port = "3000"
 	}
 
+	http.HandleFunc("/sign_up", createUser)
+	http.HandleFunc("/login", createJWT)
 	http.HandleFunc("/connect", serveWs)
 	log.Fatal(http.ListenAndServe(":"+port, httpLog(http.DefaultServeMux)))
+}
+
+// TODO: Return error if username is already taken instead of just overwriting
+// the user
+func createUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	username, password, err := decodeUsernameAndPassword(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	user := CreateUser(username, password)
+	if err := json.NewEncoder(w).Encode(&user); err != nil {
+		panic(err)
+	}
+}
+
+func createJWT(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	username, password, err := decodeUsernameAndPassword(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	user, err := db.getUserByUsername(username)
+	if err != nil {
+		panic(err)
+	}
+
+	if user.Password != password {
+		w.Write([]byte("password does not match"))
+		return
+	}
+
+	jwt := NewJWT(user.ID)
+	var resp struct {
+		Token string `json:"token"`
+	}
+	resp.Token = jwt.String()
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		panic(err)
+	}
+}
+
+func decodeUsernameAndPassword(r io.Reader) (username, password string,
+	err error) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := json.NewDecoder(r).Decode(&req); err != nil {
+		return "", "", err
+	}
+
+	return req.Username, req.Password, nil
 }
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
