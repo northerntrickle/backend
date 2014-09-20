005440f7ad110bd62129e34fbb7625fb2006f330
Zach Latta <zach@zachlatta.com>
2014-09-20 13:50:36 -0700

    Initial commit.

---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+type conn struct {
+	ws   *websocket.Conn
+	send chan []byte
+}
+
+func (c *conn) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+func (c *conn) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case msg, ok := <-c.send:
+			if !ok {
+				c.ws.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.ws.WriteMessage(websocket.TextMessage, msg); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+type hub struct {
+	conns      map[*conn]bool
+	broadcast  chan []byte
+	register   chan *conn
+	unregister chan *conn
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case c := <-h.register:
+			h.conns[c] = true
+		case c := <-h.unregister:
+			if _, ok := h.conns[c]; ok {
+				delete(h.conns, c)
+				close(c.send)
+			}
+		case m := <-h.broadcast:
+			for c := range h.conns {
+				select {
+				case c.send <- m:
+				default:
+					close(c.send)
+					delete(h.conns, c)
+				}
+			}
+		}
+	}
+}
+
+var (
+	h = &hub{
+		conns:      make(map[*conn]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *conn),
+		unregister: make(chan *conn),
+	}
+
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+func httpLog(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+		log.Printf("Completed in %s", time.Now().Sub(start).String())
+	})
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/connect", serveWs)
+	log.Fatal(http.ListenAndServe(":"+port, httpLog(http.DefaultServeMux)))
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	c := &conn{send: make(chan []byte, 256), ws: ws}
+	h.register <- c
+	c.writePump()
+}
